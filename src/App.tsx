fc4027b262f8eddab5b3eac5896b3fdd3f459c3a
SargisXachatryan <sargis.a.xachatryan@gmail.com>
2024-09-08 23:07:31 +0400

    error 404 page 2nd v

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './Pages/Home/Page';
 import { Schedule } from './Pages/Schedule/Page';
+import { Error404 } from './components/error404/Page';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/schedule" element={<Schedule />} />
+
+      <Route path="*" element={<Error404 />} />
     </Routes>
   );
 }
