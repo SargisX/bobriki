098996ac2e946114b8e3504e0d4f71c4d2ef9a71
SargisX <supersargis2007@gmail.com>
2024-10-19 00:41:35 +0400

    fixed admin bug

--- src/components/auth/authUtils.ts
+++ src/components/auth/authUtils.ts
@@ -5,27 +5,29 @@ const SESSION_KEY = "user_session"
 const USERS_KEY = "users"
 
 export const saveLoginSession = (user: SignUpUser) => {
-    const { username, password } = user
+    const { username, password } = user 
     const userData = {
         id: username === 'SargisX' ? '13795' : uuidv4(),
         username,
         password,
         role: username === "SargisX" ? "admin" : "user"
-    }
+    }   
 
-    let users = JSON.parse(localStorage.getItem(USERS_KEY) || "[]")
+    let users = JSON.parse(localStorage.getItem(USERS_KEY) || "[]") 
 
     if (!users.some((u: SignUpUser) => u.username === username)) {
-        users.push(userData)
+        users.push(userData)    
         localStorage.setItem(USERS_KEY, JSON.stringify(users))
     }
 
     const sessionData = {
         timestamp: new Date().getTime(),
         userId: userData.id,
+        role: userData.role
     }
     sessionStorage.setItem(SESSION_KEY, JSON.stringify(sessionData))
 }
+
 
 export const clearLoginSession = () => {
     sessionStorage.removeItem(SESSION_KEY)
@@ -35,7 +37,7 @@ export const isSessionValid = () => {
     const sessionData = sessionStorage.getItem(SESSION_KEY)
     if (!sessionData) return false
     const { timestamp } = JSON.parse(sessionData)
-    const expirationTime = 2 * 24 * 60 * 60 * 1000
+    const expirationTime = 7 * 24 * 60 * 60 * 1000
     return new Date().getTime() - timestamp < expirationTime
 }
 
@@ -43,3 +45,4 @@ export const getUserRole = () => {
     const sessionData = sessionStorage.getItem(SESSION_KEY)
     return sessionData ? JSON.parse(sessionData).role : null
 }
+
